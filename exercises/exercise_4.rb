54a8ac50a42a723bdcf605036c84273c6e8cc618
gavinswan <gavtswan@gmail.com>
2021-03-30 17:08:22 +0000

    Finish exercise_4 - loading a subset of stores

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,4 +7,33 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+### Exercise 4: Loading a subset of stores
 
+# 1. Borrowing and modifying the code from Exercise one, create 3 more stores:
+#   * Surrey (annual_revenue of 224000, carries women's apparel only)
+#   * Whistler (annual_revenue of 1900000 carries men's apparel only)
+#   * Yaletown (annual_revenue of 430000 carries men's and women's apparel)
+# 2. Using the `where` class method from Active Record, fetch (a collection of) only those stores that carry men's apparel. Assign the results to a variable `@mens_stores`.
+# 3. Loop through each of these stores and output their name and annual revenue on each line.
+# 4. Do another fetch but this time load stores that carry women's apparel and are generating less than $1M in annual revenue.
+
+store = Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+store = Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+store = Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+@mens_stores = Store.where(mens_apparel: true)
+
+puts "---mens_stores---"
+
+for store in @mens_stores do
+  puts "#{store.name} #{store.annual_revenue}"
+end
+
+@womens_stores = Store.where(womens_apparel: true)
+
+puts "---womens_stores---"
+
+@womens_stores.each do |store|
+  if store.annual_revenue < 1000000
+    puts "#{store.name} #{store.annual_revenue}"
+  end
+end

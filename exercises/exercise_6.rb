889fcd98c10c4f29196a2e3e379454ed831155a0
gavinswan <gavtswan@gmail.com>
2021-03-31 01:09:11 +0000

    Finish exercise_6 - one-to-many association

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,27 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+# We haven't used the Employee class (and employees table) at all yet. If you look at this table's column structure,
+# you'll find that it has a `store_id` (integer) column. This is a column that identifies which store each employee belongs to.
+# It points to the `id` (integer) column of their store.
+
+# Let's tell Active Record that these two tables are in fact related via the `store_id` column.
+
+# 1. Add the following code _directly_ inside the Store model (class): `has_many :employees`
+# 2. Add the following code directly inside the Employee model (class): `belongs_to :store`
+# 3. Add some data into employees. Here's an example of one (note how it differs from how you create stores): `@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)`
+# 4. Go ahead and create some more employees using the create method. You can do this by making multiple calls to create (like you have before.) No need to assign the employees to variables though. Create them through the `@store#` instance variables that you defined in previous exercises. Create a bunch under `@store1` (Burnaby) and `@store2` (Richmond). Eg: `@store1.employees.create(...)`.
+
+class Store < ActiveRecord::Base
+  has_many :employees
+end
+
+class Employee < ActiveRecord::Base
+  belongs_to :store
+end
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Austen", last_name: "Matthews", hourly_rate: 100)
+@store2.employees.create(first_name: "Mitch", last_name: "Marner", hourly_rate: 90)
+@store2.employees.create(first_name: "John", last_name: "Tavares", hourly_rate: 80)
+@store2.employees.create(first_name: "Morgan", last_name: "Reilly", hourly_rate: 70)
